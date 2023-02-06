1df736c4150bd051bbc091080e9a45b8db61ecee
itumejia <iturielmg@hotmail.com>
2023-02-06 19:34:08 +0100

    Uncomment user to test Users page

--- frontend/src/user/pages/Users.js
+++ frontend/src/user/pages/Users.js
@@ -3,13 +3,13 @@ import UsersList from "../components/UsersList";
 
 const Users = () => {
     const USERS = [
-        // {
-        //     id: 'u1',
-        //     name: 'james',
-        //     image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Pierre-Person.jpg/1200px-Pierre-Person.jpg',
-        //     placeCount: 1 
+        {
+            id: 'u1',
+            name: 'james',
+            image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Pierre-Person.jpg/1200px-Pierre-Person.jpg',
+            placeCount: 1 
 
-        // }
+        }
     ]
     return <UsersList items={USERS}/>
 }
