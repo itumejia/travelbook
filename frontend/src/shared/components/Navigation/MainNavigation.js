16b64312e3a1d5ed8da49be05fbfd78b54455d31
itumejia <iturielmg@hotmail.com>
2023-02-06 21:07:13 +0100

    Add side drawer for mobile navigation

--- frontend/src/shared/components/Navigation/MainNavigation.js
+++ frontend/src/shared/components/Navigation/MainNavigation.js
@@ -1,14 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import MainHeader from "./MainHeader";
 import NavLinks from "./NavLinks";
 
 import "./MainNavigation.css"
+import SideDrawer from "./SideDrawer";
+import Backdrop from "../UIElements/Backdrop";
 
 const MainNavigation = props => {
+
+    const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+
+    const openDrawer = () => {
+        setDrawerIsOpen(true);
+    }
+
+    const closeDrawer = () => {
+        setDrawerIsOpen(false);
+    }
     return (
+        <React.Fragment>
+        {
+            drawerIsOpen &&
+            <Backdrop onClick={closeDrawer}/>
+        }
+        {
+            drawerIsOpen &&
+            <SideDrawer className="main-navigation__drawer-nav">
+                <NavLinks/>
+            </SideDrawer>
+        }
         <MainHeader>
-            <button className="main-navigation__menu-btn">
+            <button className="main-navigation__menu-btn" onClick={openDrawer}>
                 <span/>
                 <span/>
                 <span/>
@@ -16,11 +39,12 @@ const MainNavigation = props => {
             <h1 className="main-navigation__title">
                 <Link to="/">TravelBook</Link>
             </h1>
-            <nav>
+            <nav className="main-navigation__header-nav">
                 <NavLinks/>
             </nav>
 
         </MainHeader>
+        </React.Fragment>
     )
 };
 
